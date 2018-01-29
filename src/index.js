aa1154262cacd767a6c48878c1900f9043b85f31
Temicka Brown <mekapleio@yahoo.com>
2018-01-29 16:28:40 -0500

    unfinished button

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
-import App from "./components/App/App.js";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App/App.js'
+import './index.css'
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
