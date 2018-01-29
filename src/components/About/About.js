0bed1c9c9f8093878c9e43da762eb6fe7e5d7b4d
Brian Reynolds <brikrey@gmail.com>
2018-01-29 16:35:50 -0500

    implemented routing in App.js

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./About.css";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h3>About</h3>
+        <p>Not part of MPV</p>
+      </div>
+    );
+  }
+}
+
+export default About;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
-import logo from "../../logo.svg";
+
+import Category from "../Category/Category.js";
+import About from "../About/About.js";
+import Todos from "../Todos/Todos.js";
+import TodoEdit from "../TodoEdit/TodoEdit.js";
 import "./App.css";
 
+import { Route, Link, Redirect, Switch } from "react-router-dom";
+
 class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   initialize state
+  //   this.state = {
+  //     buckets: []
+  //   };
+  // }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">test</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <nav>
+          <h1>To-Do-Dos</h1>
+          <Link to="/category">Category</Link>
+          <Link to="/about">About</Link>
+          <Link to="/todo">To-Do-Dos</Link>
+        </nav>
+        <main>
+          <Switch>
+            <Route path="/category" render={() => <Category />} />
+            <Route path="/about" render={() => <About />} />
+            <Route exact path="/todo" render={props => <Todos {...props} />} />
+            <Route path="/todo/:_id" component={TodoEdit} />
+            {/* <Route path="/*" render={() => <Redirect to="/todo" />} /> */}
+          </Switch>
+        </main>
       </div>
     );
   }
