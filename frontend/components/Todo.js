124067eb10dc5e196c7567831406cfc5f25c08f9
Zelalem Tolera <tolerazelalem@gmail.com>
2018-01-29 14:34:56 -0500

    add Todo and Catagory components

---
+++ frontend/components/Todo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Todo component</h1>
+      </div>
+    )
+  }
+}
+export default Todo
