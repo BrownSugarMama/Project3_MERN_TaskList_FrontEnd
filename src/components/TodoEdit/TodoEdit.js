7ba614e3d09457089a9a6f125cd70f1237b41df7
Zelalem Tolera <tolerazelalem@gmail.com>
2018-01-29 16:15:45 -0500

    add startup file to todo edit

---
+++ src/components/TodoEdit/TodoEdit.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TodoEdit extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Edit to do</h1>
+      </div>
+    )
+  }
+}
+export default TodoEdit
