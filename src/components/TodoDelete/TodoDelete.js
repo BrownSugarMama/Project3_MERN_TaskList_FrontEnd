92fb2f8e4e53f3733ceb18c4cb076e759bb6bba5
Temicka Brown <mekapleio@yahoo.com>
2018-01-30 09:47:59 -0500

    updated

--- src/components/TodoDelete/TodoDelete.js
+++ src/components/TodoDelete/TodoDelete.js
@@ -1,7 +1,19 @@
 import React, {Component} from 'react'
 import { Route, Link, Redirect, Switch } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 
 class Delete extends Component {
+  /* e.preventDefault()
+  axios
+    .delete(
+      //add link here
+    )
+    .then(( => {
+      this.props.history.push()
+    })
+
+  } */
   render () {
     return (
 
