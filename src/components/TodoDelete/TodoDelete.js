3085b0bf15c6fd4590b12793eb4d763fbc3c6d80
neechie <neechieg@gmail.com>
2018-01-30 16:55:42 -0500

    todoDelete started

--- src/components/TodoDelete/TodoDelete.js
+++ src/components/TodoDelete/TodoDelete.js
@@ -4,21 +4,11 @@ import { withRouter } from 'react-router-dom'
 import axios from 'axios'
 
 class Delete extends Component {
-  /* e.preventDefault()
-  axios
-    .delete(
-      //add link here
-    )
-    .then(( => {
-      this.props.history.push()
-    })
-
-  } */
   render () {
     return (
 
       <div>
-        <button type='button' class='deleteButton'>deleteButton'>Delete</button>
+        <button type='button' class='deleteButton'> Delete </button>
       </div>
     )
   }
