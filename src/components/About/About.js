62e33ab266e774f4df4c83c21d306e8383273f77
Temicka Brown <mekapleio@yahoo.com>
2018-02-01 16:24:08 -0500

    rough draft not finished

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,10 +4,18 @@ import './About.css'
 class About extends Component {
   render () {
     return (
+      // rough draft just outlining things about the app for now and will proof read and finish up
       <div>
         <h3>About</h3>
         <p>We built a full stack application called Todos. It is basically
           a Todo List that has reminders and updates.
+        </p>
+
+        <p>
+        So basically the Todo Task list app is an app that is a
+        cloud based reminder that can help you when you need to
+        remember things. In the todo list you have: Title,
+        Description, Importance, Category, Due Date, and Status.
         </p>
       </div>
     )
