f6df9e203cd5a0a2c57f1741a32c65ce774e2c41
Brian Reynolds <brikrey@gmail.com>
2018-02-05 23:48:01 -0500

    updated category.js styling

--- src/components/Category/Category.js
+++ src/components/Category/Category.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import "./Category.css";
+import { Container, Row, Col } from "reactstrap";
 
 class Category extends Component {
   render() {
     return (
       <div>
-        <h3>Category</h3>
-        <p>
-          Not part of MPV. This component will be used to manage list of values
-          used to categorize tasks.
-        </p>
+        <Container>
+          <Row />
+          <Row>
+            <Col xs="2" />
+            <Col xs="8">
+              Not part of MPV. This component will be used to manage list of
+              values used to categorize tasks.
+            </Col>
+            <Col xs="2" />
+          </Row>
+          <Row />
+        </Container>
       </div>
     );
   }
